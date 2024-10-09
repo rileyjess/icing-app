21f3c883338f16acffd5531c4f430d9d08c52202
MattP74 <matthewpoe101@gmail.com>
2024-10-09 18:57:36 -0500

    updated code for both index.js and order.js for models

--- models/orders.js
+++ models/orders.js
@@ -1,6 +1,7 @@
 const orders = [];
 
-export function createOrder(orderName) {
+// Function to create a new order
+function createOrder(orderName) {
     const order = {
         name: orderName,
         date: new Date().toLocaleString()
@@ -9,9 +10,13 @@ export function createOrder(orderName) {
     console.log(`Order created: ${orderName}`);
 }
 
-export function displayOrders() {
+// Function to display all current orders
+function displayOrders() {
     console.log("Current Orders:");
     orders.forEach(order => {
         console.log(`- ${order.name}: (Date: ${order.date})`);
     });
 }
+
+// Export the functions
+module.exports = { createOrder, displayOrders };
