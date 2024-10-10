c07d82ea4ff679dc62d29f3934d247a57c82fc3f
MattP74 <matthewpoe101@gmail.com>
2024-10-09 19:54:47 -0500

    made changess to schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,2 @@
--- Create the employee table
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
-);
-
--- Create the orders table
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    order_name VARCHAR(50) NOT NULL,
-    employee_id INTEGER,
-    FOREIGN KEY (employee_id) REFERENCES employee(id)
-);
+DROP DATABASE IF EXISTS icing_db;
+CREATE DATABASE icing_db;
